491ddba43849c8ad3b6648af0144dadd02a56160
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-01-06 22:16:43 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+basic.forever(function () {
+    huskylens.request()
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+})
